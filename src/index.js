b384e63b9b710d61c3616a91a22730fa5e600599
Jaakko Lehtonen <jaakko.lehtonen@cap.fi>
2023-11-22 13:34:19 +0200

    Use es6 imports to utilise dependency extraction, read deps and ver from asset file (cache busting), use eslint

--- src/index.js
+++ src/index.js
@@ -1,78 +1,84 @@
-const { __ } = wp.i18n;
-
-const { createHigherOrderComponent } = wp.compose;
-const { addQueryArgs } = wp.url;
-const { isReusableBlock } = wp.blocks; 
-const { Button } = wp.components;
-const { useDispatch } = wp.data;
+import { __ } from '@wordpress/i18n';
+import { createHigherOrderComponent } from '@wordpress/compose';
+import { addQueryArgs } from '@wordpress/url';
+import { isReusableBlock } from '@wordpress/blocks';
+import { Button } from '@wordpress/components';
+import { useDispatch } from '@wordpress/data';
 import { store as reusableBlocksStore } from '@wordpress/reusable-blocks';
+import { addFilter } from '@wordpress/hooks';
 
 import './style.scss';
 
 /**
  * Add custom HTML to reusable blocks.
  */
- const withLockedReusableBlocks = createHigherOrderComponent( ( BlockEdit ) => {
-    return ( props ) => {
+const withLockedReusableBlocks = createHigherOrderComponent( ( BlockEdit ) => {
+	return ( props ) => {
+		const { attributes, clientId } = props;
 
-        const { attributes, clientId } = props;
+		if ( props.isSelected && isReusableBlock( props ) && attributes.ref ) {
+			let showConvert = true,
+				showEdit = true;
+			if ( typeof lrb !== 'undefined' ) {
+				if (
+					lrb.hide_convert_button &&
+					lrb.hide_convert_button === '1'
+				) {
+					showConvert = false;
+				}
+				if ( lrb.hide_edit_button && lrb.hide_edit_button === '1' ) {
+					showEdit = false;
+				}
+			}
 
-        if ( props.isSelected && isReusableBlock( props ) && attributes.ref ) {
+			const { __experimentalConvertBlockToStatic: convertBlockToStatic } =
+				useDispatch( reusableBlocksStore );
 
-            let showConvert = true, showEdit = true;
-            if ( typeof lrb !== 'undefined' ) {
-                if ( lrb.hide_convert_button && '1' === lrb.hide_convert_button ) {
-                    showConvert = false;
-                }
-                if ( lrb.hide_edit_button && '1' === lrb.hide_edit_button ) {
-                    showEdit = false;
-                }
-            }
-
-            const {
-                __experimentalConvertBlockToStatic: convertBlockToStatic,
-            } = useDispatch( reusableBlocksStore );
-
-            return (
-                <div className="wp-block wp-reusable-block-locked">
-                    <div className="wp-reusable-block-locked__wrapper">
-                        { showEdit && 
-                            <Button
-                                variant="primary"
-                                className="wp-reusable-block-locked__edit-link"
-                                href={ addQueryArgs( 'post.php', {
-                                    post: attributes.ref,
-                                    action: 'edit'
-                                } ) }
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                { __( 'Edit reusable block', 'lock-reusable-blocks' ) }
-                            </Button>
-                        }
-                        { showConvert && 
-                            <Button
-                                onClick={ () => convertBlockToStatic( clientId ) }
-                                variant="primary"
-                                className="wp-reusable-block-locked__convert-link"
-                            >
-                                { __( 'Convert to regular blocks', 'lock-reusable-blocks' ) }
-                            </Button>
-                        }
-                    </div>
-                    <BlockEdit { ...props } />
-                </div>
-            );
-        } else {
-            return <BlockEdit { ...props } />
-        }
-
-        
-    };
+			return (
+				<div className="wp-block wp-reusable-block-locked">
+					<div className="wp-reusable-block-locked__wrapper">
+						{ showEdit && (
+							<Button
+								variant="primary"
+								className="wp-reusable-block-locked__edit-link"
+								href={ addQueryArgs( 'post.php', {
+									post: attributes.ref,
+									action: 'edit',
+								} ) }
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								{ __(
+									'Edit reusable block',
+									'lock-reusable-blocks'
+								) }
+							</Button>
+						) }
+						{ showConvert && (
+							<Button
+								onClick={ () =>
+									convertBlockToStatic( clientId )
+								}
+								variant="primary"
+								className="wp-reusable-block-locked__convert-link"
+							>
+								{ __(
+									'Convert to regular blocks',
+									'lock-reusable-blocks'
+								) }
+							</Button>
+						) }
+					</div>
+					<BlockEdit { ...props } />
+				</div>
+			);
+		}
+		return <BlockEdit { ...props } />;
+	};
 }, 'withLockedReusableBlock' );
 
-wp.hooks.addFilter(
-    'editor.BlockEdit',
-    'lock-reusable-blocks/with-locked-reusable-blocks',
-    withLockedReusableBlocks
+addFilter(
+	'editor.BlockEdit',
+	'lock-reusable-blocks/with-locked-reusable-blocks',
+	withLockedReusableBlocks
 );
