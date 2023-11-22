99ccf4912e7c376bfcb01d1b90c465640aef1fc2
Jaakko Lehtonen <jaakko.lehtonen@cap.fi>
2023-11-22 15:25:52 +0200

    Make sure spaces are used for indent

--- src/index.js
+++ src/index.js
@@ -13,72 +13,72 @@ import './style.scss';
  * Add custom HTML to reusable blocks.
  */
 const withLockedReusableBlocks = createHigherOrderComponent( ( BlockEdit ) => {
-	return ( props ) => {
-		const { attributes, clientId } = props;
+    return ( props ) => {
+        const { attributes, clientId } = props;
 
-		if ( props.isSelected && isReusableBlock( props ) && attributes.ref ) {
-			let showConvert = true,
-				showEdit = true;
-			if ( typeof lrb !== 'undefined' ) {
-				if (
-					lrb.hide_convert_button &&
+        if ( props.isSelected && isReusableBlock( props ) && attributes.ref ) {
+            let showConvert = true,
+                showEdit = true;
+            if ( typeof lrb !== 'undefined' ) {
+                if (
+                    lrb.hide_convert_button &&
 					lrb.hide_convert_button === '1'
-				) {
-					showConvert = false;
-				}
-				if ( lrb.hide_edit_button && lrb.hide_edit_button === '1' ) {
-					showEdit = false;
-				}
-			}
+                ) {
+                    showConvert = false;
+                }
+                if ( lrb.hide_edit_button && lrb.hide_edit_button === '1' ) {
+                    showEdit = false;
+                }
+            }
 
-			const { __experimentalConvertBlockToStatic: convertBlockToStatic } =
+            const { __experimentalConvertBlockToStatic: convertBlockToStatic } =
 				useDispatch( reusableBlocksStore );
 
-			return (
-				<div className="wp-block wp-reusable-block-locked">
-					<div className="wp-reusable-block-locked__wrapper">
-						{ showEdit && (
-							<Button
-								variant="primary"
-								className="wp-reusable-block-locked__edit-link"
-								href={ addQueryArgs( 'post.php', {
-									post: attributes.ref,
-									action: 'edit',
-								} ) }
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								{ __(
-									'Edit reusable block',
-									'lock-reusable-blocks'
-								) }
-							</Button>
-						) }
-						{ showConvert && (
-							<Button
-								onClick={ () =>
-									convertBlockToStatic( clientId )
-								}
-								variant="primary"
-								className="wp-reusable-block-locked__convert-link"
-							>
-								{ __(
-									'Convert to regular blocks',
-									'lock-reusable-blocks'
-								) }
-							</Button>
-						) }
-					</div>
-					<BlockEdit { ...props } />
-				</div>
-			);
-		}
-		return <BlockEdit { ...props } />;
-	};
+            return (
+                <div className="wp-block wp-reusable-block-locked">
+                    <div className="wp-reusable-block-locked__wrapper">
+                        { showEdit && (
+                            <Button
+                                variant="primary"
+                                className="wp-reusable-block-locked__edit-link"
+                                href={ addQueryArgs( 'post.php', {
+                                    post: attributes.ref,
+                                    action: 'edit',
+                                } ) }
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                { __(
+                                    'Edit reusable block',
+                                    'lock-reusable-blocks',
+                                ) }
+                            </Button>
+                        ) }
+                        { showConvert && (
+                            <Button
+                                onClick={ () =>
+                                    convertBlockToStatic( clientId )
+                                }
+                                variant="primary"
+                                className="wp-reusable-block-locked__convert-link"
+                            >
+                                { __(
+                                    'Convert to regular blocks',
+                                    'lock-reusable-blocks',
+                                ) }
+                            </Button>
+                        ) }
+                    </div>
+                    <BlockEdit { ...props } />
+                </div>
+            );
+        }
+        return <BlockEdit { ...props } />;
+    };
 }, 'withLockedReusableBlock' );
 
 addFilter(
-	'editor.BlockEdit',
-	'lock-reusable-blocks/with-locked-reusable-blocks',
-	withLockedReusableBlocks
+    'editor.BlockEdit',
+    'lock-reusable-blocks/with-locked-reusable-blocks',
+    withLockedReusableBlocks,
 );
